6228332d466cea120a8720b6364f5ca3309aca86
workplacePJ <workplace.pj@proton.me>
2024-08-09 07:56:28 +0900

    Update judgment_of_postal_code.py

--- Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
+++ Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
@@ -1,1 +1,6 @@
-
+def judgment_of_postal_code(value: str) -> bool:
+    pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9-]{8,}).*$')
+    if pattern__judgment_of_postal_code.search(value):
+        return True
+    else:
+        return False
