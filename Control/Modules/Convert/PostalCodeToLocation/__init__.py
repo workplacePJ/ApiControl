93f55f84c13a5d57d791a8d291d4c12f8ddbc7de
workplacePJ <workplace.pj@proton.me>
2024-08-09 08:53:34 +0900

    Update __init__.py

--- Control/Modules/Convert/PostalCodeToLocation/__init__.py
+++ Control/Modules/Convert/PostalCodeToLocation/__init__.py
@@ -1,2 +1,7 @@
+from typing import Any, Pattern, Literal
+import re
+import unicodedata
+import aiohttp
+import asyncio
 from .convert_postal_code_to_location import convert_postal_code_to_location
 from .judgment_of_postal_code import judgment_of_postal_code
