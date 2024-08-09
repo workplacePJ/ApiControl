788583941a386cb70906e5f176030d535d11e97c
workplacePJ <workplace.pj@proton.me>
2024-08-09 15:10:31 +0900

    Update control.py

--- Control/control.py
+++ Control/control.py
@@ -12,18 +12,18 @@ async def control(requested_values: list[dict[str, str]], **kwargs) -> list[dict
         for requested_value in requested_values:
             if "postal_code" in requested_value:
                 value: str = requested_value.get('postal_code')
-                tasks.append(convert_postal_code_to_location(session, value, KEY = kwargs.get('POSTCODE_JP_API_KEY', '')))
-                await asyncio.sleep(2)
+                tasks.append(convert_postal_code_to_location(session, value, POSTCODE_JP_API_KEY = kwargs.get('POSTCODE_JP_API_KEY', '')))
+                #await asyncio.sleep(2)
             
             elif "address" in requested_value:
                 value: str = requested_value.get('address')
-                tasks.append(convert_location_to_postal_code(session, "address", value, KEY = kwargs.get('GOOGLE_MAPS_API_KEY', '')))
+                tasks.append(convert_location_to_postal_code(session, "address", value, GOOGLE_MAPS_API_KEY = kwargs.get('GOOGLE_MAPS_API_KEY', '')))
             """
             elif "landmark" in requested_value:
                 value: str = requested_value.get('landmark')
-                tasks.append(convert_location_to_postal_code(session, "landmark", value))
+                tasks.append(convert_location_to_postal_code(session, "landmark", value, GOOGLE_MAPS_API_KEY = kwargs.get('GOOGLE_MAPS_API_KEY', '')))
             """
         results = await asyncio.gather(*tasks)
     
     return results
-#asyncio.run(control([{ "postal_code" : "1008111" }, { "postal_code" : "115-0052" }, { "postal_code" : "9812114" }], KEY = userdata.get("GOOGLE_MAPS_API_KEY")))
+#asyncio.run(control([{ "postal_code" : "1008111" }], POSTCODE_JP_API_KEY = userdata.get('POSTCODE_JP_API_KEY')))
