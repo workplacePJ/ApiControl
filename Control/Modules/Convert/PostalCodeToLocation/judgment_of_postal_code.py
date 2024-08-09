7d138091bed4d55613c4d0338115619031d2fc8d
workplacePJ <workplace.pj@proton.me>
2024-08-09 11:14:01 +0900

    Update judgment_of_postal_code.py

--- Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
+++ Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
@@ -1,6 +1,6 @@
 def judgment_of_postal_code(value: str) -> bool:
-    pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9-]{8,}).*$|^(?=.*[０-９]{3}−[０-９]{4})(?=.*[０-９−]{8})(?!.*[０-９−]{9,}).*$|^(?=.*[０-９]{7})(?!.*[０-９]{8,}).*$')
-    if pattern__judgment_of_postal_code.search(value):
+    pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9-]{8,}).*$')
+    if pattern__judgment_of_postal_code.search(unicodedata.normalize('NFKC', value)):
         return True
     else:
         return False
