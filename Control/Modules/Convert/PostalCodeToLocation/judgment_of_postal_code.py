b72c2650b0ed8fc56431aacceb8c25439845e98b
workplacePJ <workplace.pj@proton.me>
2024-08-09 09:42:02 +0900

    Update judgment_of_postal_code.py

--- Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
+++ Control/Modules/Convert/PostalCodeToLocation/judgment_of_postal_code.py
@@ -1,5 +1,5 @@
 def judgment_of_postal_code(value: str) -> bool:
-    pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9-]{8,}).*$')
+    pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9]{8,}).*$|^(?=.*[０-９]{3}−[０-９]{4})(?=.*[０-９−]{8})(?!.*[０-９−]{9,}).*$|^(?=.*[０-９]{7})(?!.*[０-９]{8,}).*$')
     if pattern__judgment_of_postal_code.search(value):
         return True
     else:
