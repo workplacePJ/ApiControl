ca9623c5aa79afda6c8074db3617ec975bd94677
workplacePJ <workplace.pj@proton.me>
2024-08-11 00:48:03 +0900

    Update judgment_of_postal_code.py

--- control/modules/convert/postal_code_to_location/judgment_of_postal_code.py
+++ control/modules/convert/postal_code_to_location/judgment_of_postal_code.py
@@ -1,4 +1,8 @@
 def judgment_of_postal_code(value: str) -> bool:
+    from typing import Pattern
+    import re
+    import unicodedata
+    
     pattern__judgment_of_postal_code: Pattern[str] = re.compile(r'^(?=.*[0-9]{3}-[0-9]{4})(?=.*[0-9-]{8})(?!.*[0-9-]{9,}).*$|^(?=.*[0-9]{7})(?!.*[0-9-]{8,}).*$')
     if pattern__judgment_of_postal_code.search(unicodedata.normalize('NFKC', value)):
         return True
