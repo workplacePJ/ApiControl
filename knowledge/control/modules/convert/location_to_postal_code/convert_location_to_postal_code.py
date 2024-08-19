0d771e55e59634a310721da9348324a3ded9fa9f
workplacePJ <workplace.pj@proton.me>
2024-08-19 14:25:14 +0900

    Update convert_location_to_postal_code.py

--- knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
+++ knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
@@ -13,8 +13,11 @@ async def convert_location_to_postal_code(session, location_type: Literal["addre
         Google maps Platform
         https://console.cloud.google.com/google/maps-apis/api-list?project=api-project-430803&authuser=2&hl=ja
     """
+    
+    # generate "result"data
+    result: dict = {}
 
-
+https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC7dLXM_6HyFxVvLVPnCLnV2uTdwqgYOKM&language=ja&address=%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%9D%BF%E6%A9%8B%E5%8C%BA%E8%B5%A4%E7%BE%BD
     
     import unicodedata
     from typing import Pattern
