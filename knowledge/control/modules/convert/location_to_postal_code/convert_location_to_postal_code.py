1e3e4ee93ea9a307f33f8630233d08860c2b8220
workplacePJ <workplace.pj@proton.me>
2024-08-19 13:25:55 +0900

    Update convert_location_to_postal_code.py

--- knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
+++ knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
@@ -3,7 +3,18 @@ import re
 import unicodedata
 
 async def convert_location_to_postal_code(session, location_type: Literal["address", "landmark"], location: str, **kwargs) -> dict[str, None | int | bool | str | list[dict[str, str | dict[str, str]]]]:
-
+    """
+    Converts a location to an postal code using a geo API.
+    Args:
+        session: aiohttp.ClientSession()
+        location_type: Location type
+        location: The location to convert.
+    Returns:
+        A dictionary containing the postal code information or error information if an error occurs.
+    API used:
+        Google maps Platform
+        https://console.cloud.google.com/google/maps-apis/api-list?project=api-project-430803&authuser=2&hl=ja
+    """
 
 if __name__=="__main__":
     import asyncio
