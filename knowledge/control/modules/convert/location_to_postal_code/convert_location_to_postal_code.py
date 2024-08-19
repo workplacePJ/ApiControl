116887f255658dae01a0cdedde58a6a9421aad09
workplacePJ <workplace.pj@proton.me>
2024-08-19 13:25:08 +0900

    Update convert_location_to_postal_code.py

--- knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
+++ knowledge/control/modules/convert/location_to_postal_code/convert_location_to_postal_code.py
@@ -1,1 +1,10 @@
+from typing import Any, Pattern, Literal
+import re
+import unicodedata
 
+async def convert_location_to_postal_code(session, location_type: Literal["address", "landmark"], location: str, **kwargs) -> dict[str, None | int | bool | str | list[dict[str, str | dict[str, str]]]]:
+
+
+if __name__=="__main__":
+    import asyncio
+    import aiohttp
